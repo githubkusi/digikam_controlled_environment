580225afac6bb767cb7e5ed3629cd15914ef630e
Markus Leuthold <git@titlis.org>
2018-07-03 01:57:35 +0200

    photosharing

---
+++ sql/create-photosharing.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS PhotoSharing;
+CREATE TABLE PhotoSharing
+(imageid INTEGER PRIMARY KEY,
+    remoteid LONGTEXT,
+    CONSTRAINT PhotoSharing_Images FOREIGN KEY (imageid) REFERENCES Images (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    mtime_tags TIMESTAMP);
