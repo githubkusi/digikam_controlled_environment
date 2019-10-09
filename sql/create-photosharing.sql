189e083d1db97462d8d1f822183391f20e0b74f1
Markus Leuthold <git@titlis.org>
2019-10-10 00:03:07 +0200

    add comments

--- sql/create-photosharing.sql
+++ sql/create-photosharing.sql
@@ -3,4 +3,4 @@ CREATE TABLE PhotoSharing
 (imageid INTEGER PRIMARY KEY,
     remoteid LONGTEXT,
     CONSTRAINT PhotoSharing_Images FOREIGN KEY (imageid) REFERENCES Images (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    mtime_tags TIMESTAMP);
+    mtime_tags TIMESTAMP, mtime_comments);
